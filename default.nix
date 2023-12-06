37d0577608fc1c71589a531622dba0b93449218a
LAPTOP-G43MA5MO\ekarni <eyalk5@gmail.com>
2023-12-06 04:49:14 +0200

    nix change

--- default.nix
+++ default.nix
@@ -1,20 +1,27 @@
 { pkgs ? import <nixpkgs> { }, pythonPackages ? pkgs.python3Packages }:
 
 let
-  mach-nix = import (builtins.fetchGit {
-    url = "https://github.com/DavHau/mach-nix";
-    ref = "refs/tags/3.5.0";
-  }) {};
-in 
+  pythonChess = pythonPackages.buildPythonPackage rec {
+    pname = "chess";
+    version = "1.10";
+    format = "wheel";
+    src = pythonPackages.fetchPypi {
+      inherit pname version;
+      sha256 = "15cfcb738d2518daf04d34b23419bd359cbd8e09da50778ebac521774fc8";
+       format = "wheel";
+      python = "py3";
+        abi = "none";
+        platform = "any";
+    };
+  };
+in
 pkgs.mkShell {
   buildInputs = [
-      pkgs.stockfish
-      (mach-nix.mkPython {
-        requirements = ''
-          python-chess
-          numpy
-          jupyter
-        '';
-      })
+    pythonPackages.pip
+    pythonPackages.numpy
+    pythonPackages.scipy
+    pythonPackages.jupyterlab
+    pkgs.stockfish
+    pythonChess
   ];
 }
